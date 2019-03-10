3d8ed0a85e1c1823a89e9b561a84b0eec067ba82
Aodeman <2432485165@qq.com>
2019-03-10 13:20:19 +0800

    the version of need pthread exit

--- server/server.c
+++ server/server.c
@@ -15,6 +15,10 @@
 #include <string.h>
 #define BACKLOG 13
 
+struct _ctr_pthread{
+    int arg_fd;
+    int p_over;
+};
 int g_cilentstop=0;
 void printfhelp(void)
 {
@@ -48,30 +52,33 @@ void pthread_set(pthread_attr_t *thread_attr)
                 printf("pthread_attr_setdetachstate() failure: %s\n", strerror(errno));//线程属性分离
 		goto CleanUp;        
 	}
+        printf("pthread_set ok!\n");
 CleanUp:
 	 pthread_attr_destroy(thread_attr);
 }
 
 void *datadell(void *arg)
 {
-	int cli_fd=(int)arg;
+	struct _ctr_pthread *p_temp=(struct _ctr_pthread *)arg;
+    p_temp->arg_fd;
+    p_temp->p_over;
 	char buff[1024];
-	int rv;
-	if((rv=read(cli_fd,buff,sizeof(buff))) < 0)
+	int rv=-1;
+	if((rv=read(p_temp->arg_fd,buff,sizeof(buff))) < 0)
 	{
-		printf("read failure:%s",strerror(errno));
-		close(cli_fd);
+		printf("read failure:%s\n",strerror(errno));
+		close(p_temp->arg_fd);
 		pthread_exit(NULL);
 	}
 	else if(rv==0)
 	{
-		printf("the socket has disconnect");
-		close(cli_fd);
+		printf("the socket has disconnect\n");
+		close(p_temp->arg_fd);
 		pthread_exit(NULL);
 	}
 	else if(rv>0)
 	{
-		printf("read data from client :%s",buff);
+		printf("read data from client :%s\n",buff);
 	}
 
 	char p_device[20];
@@ -81,10 +88,10 @@ void *datadell(void *arg)
 	char *ptemp2=NULL;
 	ptemp=strstr(buff,"/");
 	ptemp++;
-	strncpy(p_device,buff,strlen(buff)-strlen(ptemp));
+	strncpy(p_device,buff,strlen(buff)-strlen(ptemp)-1);
 	ptemp2=strstr(ptemp,"/");
 	ptemp2++;
-	strncpy(p_time,ptemp,strlen(ptemp)-strlen(ptemp2));
+	strncpy(p_time,ptemp,strlen(ptemp)-strlen(ptemp2)-1);
 	strncpy(p_tem,ptemp2,strlen(ptemp2));
 	printf("get the temprature is %s\n",ptemp2);
 
@@ -92,6 +99,7 @@ void *datadell(void *arg)
 	char *zErrMsg = 0;
 	int rc;
 	char * sql;
+    char buf[1024];
 	rc = sqlite3_open("wujinlong.db", &db);
 	if( rc )
 	{
@@ -103,7 +111,8 @@ void *datadell(void *arg)
 		fprintf(stderr, "Opened database successfully\n");
 	}
 
-	/* Create SQL statement */ sql = "CREATE TABLE RPI_temdata("  
+	/* Create SQL statement */
+    sql = "CREATE TABLE if not exists RPI_temdata("  
          "ID INT PRIMARY KEY," 
          "DEVICE           CHAR(30)    NOT NULL," 
          "TIME        CHAR(30)," 
@@ -120,18 +129,16 @@ void *datadell(void *arg)
 	{
 		fprintf(stdout, "Table created successfully\n");
 	}   
-	sql = "INSERT INTO RPI_temdata (DEVICE,TIME,TEMPRATURE) "  
-         "VALUES (p_device,p_time,atof(p_tem)); " ;
+    snprintf(buf,sizeof(buf),"INSERT INTO RPI_temdata (DEVICE,TIME,TEMPRATURE) VALUES('%s','%s',%f);",p_device,p_time,atof(p_tem));
+    sql = buf;
 	/*  
-         "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY) "  
-         "VALUES (2, 'Allen', 25, 'Texas', 15000.00 ); "     
-         "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY)" 
-         "VALUES (3, 'Teddy', 23, 'Norway', 20000.00 );" 
          "INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY)" 
          "VALUES (4, 'Mark', 25, 'Rich-Mond ', 65000.00 );";
 	 */
 
 	/* Execute SQL statement */
+
+    printf("char *sql=%s\n",sql);
 	rc = sqlite3_exec(db, sql, 0, 0, &zErrMsg);
 	if( rc != SQLITE_OK )
 	{
@@ -143,67 +150,77 @@ void *datadell(void *arg)
 		fprintf(stdout, "Records created successfully\n");
 	}	
 	sqlite3_close(db);
-	close(cli_fd);
+	close(p_temp->arg_fd);
+    p_temp->p_over=1;
+    printf("pthread exit now ...\n");
 	pthread_exit(NULL);//一般用于线程内部退出
 }
 
 int main(int argc,char *argv[])
 
 {
-        extern char *optarg;
-        extern int optind, opterr, optopt;
-        int ret;
-	int LISTENPORT;
+    /*if(daemon(0,0) < 0)
+    {
+        printf("program use daemon fafailure:%s\n",strerror(errno));
+    }*/
+    extern char *optarg;
+    extern int optind, opterr, optopt;
+    int ret;
+    int LISTENPORT;
 
-        while ((ret = getopt(argc, argv, "p:h::")) != -1)
-        {
-                switch(ret)
-                {
-                        case 'p':
-                                LISTENPORT=atoi(optarg);//printf("argv is %s\n",optarg);
-                                break;
-                        case ':':
-                                printf("option:%c missing argument\n",optopt);
-                                break;
-                        case 'h':
-                                printfhelp();
-                                break;
-                        default:
-                                printf("please enter the correct argument,-h for help\n");
+    while ((ret = getopt(argc, argv, "p:h::")) != -1) 
+    {   
+        switch(ret)
+        {   
+            case 'p':
+                LISTENPORT=atoi(optarg);
+                //printf("argv is %s\n",optarg);
+                break;
+            case ':':
+                printf("option:%c missing argument\n",optopt);
+                break;
+            case 'h':
+                printfhelp();
+                break;
+            default:
+                printf("please enter the correct argument,-h for help\n");
+        }   
+    }
+    int listen_fd;
+    struct sockaddr_in serveraddr;
+    serveraddr.sin_family=AF_INET;//协议族
+    serveraddr.sin_port=htons(LISTENPORT);  //端口绑定
+    serveraddr.sin_addr.s_addr=INADDR_ANY;
+    bzero(&(serveraddr.sin_zero), 8);
 
-                }
-        }
+    struct _ctr_pthread ctr_pthread;
+    struct _ctr_pthread *p_arg=&ctr_pthread;
+    int new_fd=-1;
+    struct sockaddr_in client_addr;
+    socklen_t addrlen;
 
-        signal(SIGUSR2, sig_usr);
-        int listen_fd;
-        struct sockaddr_in serveraddr;
-        serveraddr.sin_family=AF_INET;//协议族
-        serveraddr.sin_port=htons(LISTENPORT);  //端口绑定
-        serveraddr.sin_addr.s_addr=INADDR_ANY;
-        bzero(&(serveraddr.sin_zero), 8);
-        listen_fd = socket(AF_INET,SOCK_STREAM,0);
+    signal(SIGUSR2, sig_usr);
 
-        if(listen_fd < 0)
-        {
-                printf("creat socket failure:%s",strerror(errno));
-		return -1;
-        }
+    listen_fd = socket(AF_INET,SOCK_STREAM,0);
+    if(listen_fd < 0)
+    {
+        printf("creat socket failure:%s\n",strerror(errno));
+        return -1;
+    }
 
 	if(bind(listen_fd,(struct sockaddr *)&serveraddr,sizeof(serveraddr)) < 0)
 		//int bind(int sockfd, const struct sockaddr *addr,socklen_t addrlen);
 	{
-		printf("bind failure:%s",strerror(errno));
+		printf("bind failure:%s\n",strerror(errno));
 		return -2;
 	}
+
 	if(listen(listen_fd,BACKLOG) < 0)//int listen(int sockfd, int backlog);
 	{
-		printf("listen failure:%s",strerror(errno));
+		printf("listen failure:%s\n",strerror(errno));
 		return -3;
 	}
-	int new_fd;
-	struct sockaddr_in client_addr;
-	socklen_t addrlen;
-	printf("succeed 1!\n");
+
 	while(!g_cilentstop)
 	{
 		printf("Waitting for client connection...\n");
@@ -211,8 +228,9 @@ int main(int argc,char *argv[])
 		//int accept(int sockfd,struct sockaddr *addr,socklen_t *addrlen);
 		if(new_fd<0)
 		{
-			printf("accept a new client failure;%s",strerror(errno));
-			return -4;
+			printf("accept a new client failure;%s\n",strerror(errno));
+            continue;
+			//return -4;
 		}
 		printf("accept a new cilent:fd[%d]\n",new_fd);
 				//new_fd,inet_ntoa(client_addr.sin_addr),ntohs(client_addr.sin_port));
@@ -220,7 +238,12 @@ int main(int argc,char *argv[])
 		pthread_attr_t attr;
 		pthread_t tid;
 		pthread_set(&attr);
-		pthread_create(&tid,&attr,datadell,(void *)&new_fd);
+        p_arg->arg_fd=new_fd;
+        p_arg->p_over=0;
+		if(pthread_create(&tid,&attr,datadell,(void *)p_arg) < 0)
+        {
+            printf("creat a pthread failure:%s\n",strerror(errno));
+        }
 		/*
 		   
 		 int ped_create(
@@ -230,8 +253,14 @@ int main(int argc,char *argv[])
                  void *restrict_arg //默认为NULL。若上述函数需要参数，将参数放入结构中并将地址作为arg传入。
                   );
 		 */
-		close(new_fd);
+          /* while(!(p_arg->p_over))
+           {
+               sleep(1);
+               printf("wait pthread exit...\n");
+           }*/
 	}
+    close(listen_fd);
+    return 0;
 
 }
 
