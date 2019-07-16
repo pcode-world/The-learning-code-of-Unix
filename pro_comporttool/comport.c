4c33c465f3193698a7c64fc718c384361674f83e
wujinlong <547124558@qq.com>
2019-07-16 20:04:13 +0800

    modify by wu

--- pro_comporttool/comport.c
+++ pro_comporttool/comport.c
@@ -1,101 +1,127 @@
-#include<stdlib.h>
-#include <stdio.h>
-#include <termios.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include<errno.h>
-#include <strings.h>
-#include <string.h>
 #include "comport.h"
-int openComport(comport *p_comport,struct termios *term)
-{
-	int serial_fd = -1;
-	if((serial_fd = open(p_comport->path,O_RDWR | O_NOCTTY | O_NDELAY)) < 0)
-	{
-		printf("open a serialport failure:%s\n",strerror(errno));
-		_exit(-1);
-	}
-	if(isatty(serial_fd) == 0)
-	{
-		printf("open fd is not a terminal device\n");
-		_exit(-1);
-	}
 
-	p_comport->com_fd = serial_fd;
-	if(tcgetattr(serial_fd,term) < 0)
-	{
-		printf("tcgetattr failure:%s\n",strerror(errno));
-		return -3;
-	}
-	
+comport *initComport()
+{
+    comport *comport_info = NULL;
+
+    if((comport_info = (comport *)malloc(sizeof(comport))) == NULL)
+    {
+        return NULL;
+    }
+    memset(comport_info,0,sizeof(comport));
+    comport_info->paritybit='N';
+    comport_info->com_fd = -1;
+    comport_info->databit = 8;
+    comport_info->baudrate =115200;
+    comport_info->stopbit = 1;
+    comport_info->isopen = 0;
+    return comport_info;
 }
 
-int _set_baudrate(struct termios *term,speed_t baudspeed)
+void comport_term(comport *pcomport)
 {
-	if(cfsetispeed(term,baudspeed) < 0)
+    if(NULL == pcomport)
+    {
+        return;
+    }
+
+    if(!pcomport->isopen)
+    {
+        close(pcomport->com_fd);
+    }
+
+    free(pcomport);
+    pcomport = NULL;
+}
+
+int _set_baudrate(struct termios *term,speed_t baudrate)
+{
+	if(cfsetispeed(term,baudrate) < 0)
 	{
 		printf("cfsetispeed failure:%s\n",strerror(errno));
 		return -1;
 	}
-	if(cfsetospeed(term,baudspeed) < 0)
+
+	if(cfsetospeed(term,baudrate) < 0)
 	{
 		printf("cfsetospeed failure:%s\n",strerror(errno));
 		return -2;
 	}
+    return 0;
 }
-void set_baudrate(int baudspeed,struct termios *term)
+
+void set_baudrate(int baudrate,struct termios *term)
 {
 	/*0 50 75 110 134 150 200 300 600 1200 1800 2400 4800 9600 19200 38400 57600 115200 230400*/
-	switch(baudspeed)
+	switch(baudrate)
 	{
 		case 0:
-			_set_baudrate(term, B0);break;
+			_set_baudrate(term, B0);
+            break;
 		case 50:
-			_set_baudrate(term, B50);break;
+			_set_baudrate(term, B50);
+            break;
 		case 75:
-			_set_baudrate(term, B75);break;
+			_set_baudrate(term, B75);
+            break;
 		case 110:
-			_set_baudrate(term, B110);break;
+			_set_baudrate(term, B110);
+            break;
 		case 134:
-			_set_baudrate(term, B134);break;
+			_set_baudrate(term, B134);
+            break;
 		case 150:
-			_set_baudrate(term, B150);break;
+			_set_baudrate(term, B150);
+            break;
 		case 200:
-			_set_baudrate(term, B200);break;
+			_set_baudrate(term, B200);
+            break;
 		case 300:
-			_set_baudrate(term, B300);break;
+			_set_baudrate(term, B300);
+            break;
 		case 600:
-			_set_baudrate(term, B600);break;
+			_set_baudrate(term, B600);
+            break;
 		case 1200:
-			_set_baudrate(term, B1200);break;
+			_set_baudrate(term, B1200);
+            break;
 		case 1800:
-			_set_baudrate(term, B1800);break;
+			_set_baudrate(term, B1800);
+            break;
 		case 2400:
-			_set_baudrate(term, B2400);break;
+			_set_baudrate(term, B2400);
+            break;
 		case 4800:
-			_set_baudrate(term, B4800);break;
+			_set_baudrate(term, B4800);
+            break;
 		case 9600:
-			_set_baudrate(term, B9600);break;
+			_set_baudrate(term, B9600);
+            break;
 		case 19200:
-			_set_baudrate(term, B19200);break;
+			_set_baudrate(term, B19200);
+            break;
 		case 38400:
-			_set_baudrate(term, B38400);break;
+			_set_baudrate(term, B38400);
+            break;
 		case 57600:
-			_set_baudrate(term, B57600);break;
+			_set_baudrate(term, B57600);
+            break;
 		case 115200:
-			_set_baudrate(term, B115200);break;
+			_set_baudrate(term, B115200);
+            break;
 		case 230400:
-			_set_baudrate(term, B230400);break;
+			_set_baudrate(term, B230400);
+            break;
 		default:
-			_set_baudrate(term, B115200);break;
+			_set_baudrate(term, B115200);
+            break;
 	}
 }
-void set_data_bit(int datab,struct termios *term)
+
+void set_data_bit(int databit,struct termios *term)
 {
 	term->c_cflag &= ~CSIZE;
-	switch (datab)
+	switch (databit)
 	{
 		case 8:
 			term->c_cflag |= CS8;
@@ -115,9 +141,10 @@ void set_data_bit(int datab,struct termios *term)
 	}
 
 }
-void set_parity(char parityb,struct termios *term)
+
+void set_parity(char paritybit,struct termios *term)
 {
-	switch (parityb) 
+	switch (paritybit) 
 	{
 		case 'N':                  /* no parity check */
 			term->c_cflag &= ~PARENB;
@@ -136,12 +163,14 @@ void set_parity(char parityb,struct termios *term)
 	}
 
 }
-void set_stopbit(int stopb,struct termios *term)
+
+void set_stopbit(int stopbit,struct termios *term)
 {
-	if (2 == stopb)
+	if (2 == stopbit)
 	{
 		term->c_cflag |= CSTOPB;  /* 2 stop bits */
 	} 
+
 	else
 	{
 		term->c_cflag &= ~CSTOPB; /* 1 stop bit */
@@ -152,32 +181,91 @@ void set_stopbit(int stopb,struct termios *term)
 int setComport(comport *p_comport,struct termios *term)
 {
     term->c_oflag &= ~OPOST;
-	set_stopbit(p_comport->stopb,term);
-	set_parity(p_comport->parityb,term);
-	set_data_bit(p_comport->datab,term);
-	set_baudrate(p_comport->baudspeed,term);
+    term->c_cflag |= CLOCAL | CREAD;//用于本地连接和接收使用
+    term->c_oflag = 0; //输出模式 
+    term->c_lflag = 0; //不激活终端模式
+	set_stopbit(p_comport->stopbit,term);
+	set_parity(p_comport->paritybit,term);
+	set_data_bit(p_comport->databit,term);
+	set_baudrate(p_comport->baudrate,term);
 	tcflush(p_comport->com_fd,TCIOFLUSH);
+
 	if((tcsetattr(p_comport->com_fd,TCSANOW,term)) != 0)
 	{
 		printf("tcsetattr failure:%s\n",strerror(errno));
 		return -1;
 	}
-    printf("set comport ok!\n");
-    printf("The details are printed below\n");
-    system("sudo sudo stty -F /dev/ttyUSB0 -a");
 
+    printf("set comport ok!\n");
+    //printf("The details are printed below\n");
+    //system("sudo sudo stty -F /dev/ttyUSB0 -a");
+    return 0;
+
+}
+
+int openComport(comport *p_comport,struct termios *old_term)
+{
+    struct termios new_term;
+	int serial_fd = -1;
+
+    if((NULL == p_comport) && (NULL == old_term))
+    {
+        return -1;
+    }
+
+    if(p_comport->isopen)
+    {
+        closeComport(p_comport,old_term);
+    }
+
+	if((serial_fd = open(p_comport->path,O_RDWR | O_NOCTTY | O_NDELAY)) < 0)
+	{
+		printf("open a serialport failure:%s\n",strerror(errno));
+        return -1;
+	}
+
+
+	if(isatty(serial_fd) == 0)
+	{
+		printf("open fd is not a terminal device\n");
+        close(serial_fd);
+        return -1;
+	}
+
+	p_comport->com_fd = serial_fd;
+    p_comport->isopen = 1;
+
+    if(tcgetattr(serial_fd,old_term) < 0)
+	{
+		printf("tcgetattr failure:%s\n",strerror(errno));
+		return -3;
+	}
+
+    memset(&new_term,0,sizeof(new_term));
+
+    if(tcgetattr(serial_fd,&new_term) < 0)
+	{
+		printf("tcgetattr failure:%s\n",strerror(errno));
+		return -3;
+    }
+
+    setComport(p_comport,&new_term);
+    return 0;
+	
 }
 
 int readComport(int fd,char *buff,int buffsize)
 {
-    memset(buff,0,buffsize);
+	int rv = -1;
+
 	if(buff == NULL)
 	{
 		printf("Cannot pass in null pointer\n");
 		return -1;
-	}
-	int rv = -1;
-	if((rv = read(fd,buff,buffsize)) < 0)
+    }
+    
+    memset(buff,0,buffsize);
+    if((rv = read(fd,buff,buffsize)) < 0)
 	{
 		printf("read failure:%s\n",strerror(errno));
 		return -1;
@@ -187,26 +275,42 @@ int readComport(int fd,char *buff,int buffsize)
 
 int writeComport(int fd,char *buff,int buffsize)
 {
+	int rv = -1;
+
 	if(buff == NULL)
 	{
 		printf("Cannot pass in null pointer\n");
 		return -1;
 	}
-	int rv = -1; 
+
 	if((rv = write(fd,buff,buffsize)) < 0)
 	{
 		printf("write failure:%s\n",strerror(errno));
+        return -1;
 	}
-	printf("write data to serialport:\n%s",buff);
+
     usleep(10*1000);
 	return rv;
 }
-int closeComport(int fd,struct termios *old_term)
+
+int closeComport(comport *pcomport,struct termios *old_term)
 {
-	tcflush(fd,TCIOFLUSH);
-	if((tcsetattr(fd,TCSANOW,old_term)) != 0)
+    if(pcomport == NULL && old_term == NULL)
+    {
+        return -1;
+    }
+
+	tcflush(pcomport->com_fd,TCIOFLUSH);
+
+	if((tcsetattr(pcomport->com_fd,TCSANOW,old_term)) != 0)
 	{
 		printf("Set to the original property failure:%s\n",strerror(errno));
+        return -1;
 	}
-	close(fd);
+
+    pcomport->isopen = 0;
+	close(pcomport->com_fd);
+    free(pcomport);
+    pcomport = NULL;
+    return 0;
 }	
--- pro_comporttool/main.c
+++ pro_comporttool/main.c
@@ -1,74 +1,62 @@
 #include "comport.h"
+#include "comport.c"
 #include <stdlib.h>
 #include <signal.h>
 #include <getopt.h>
-#define ARRMAX 64
 #include <stdio.h>
 #include <unistd.h>
 #include<errno.h>
 #include <string.h>
-static int sig_exit = 1;
+#define ARRMAX 64
+static int sig_exit = 0;
 
 void showusage()
 {
     printf("********This is the instruction manual**********\n");
     printf("option\n");
     printf("-p -b -d -s cannot be followed by Spaces !!!!!!!!\n");
-    printf("-p --parityb  ->Specifies parity checking,Default no check\n");
-    printf("-b --baudspeed   ->Specified baud rate,Default 115200\n");
-    printf("-d --datab   ->Specified databit,Default 8\n");
-    printf("-s --stopb   ->Specified stopbit,Default 1\n");
+    printf("-p --paritybit  ->Specifies parity checking,Default no check\n");
+    printf("-b --baudrate   ->Specified baud rate,Default 115200\n");
+    printf("-d --databit   ->Specified databit,Default 8\n");
+    printf("-s --stopbit   ->Specified stopbit,Default 1\n");
     printf("-f --path    ->Specified The path of the serial device\n");
     printf("-h --help  ->print the usage\n");
 }
-void jugnu(char *p)
-{
-    if((' ' == *p) || (*p < '0') || (*p > ':'))
-    {
-        printf("Options cannot be followed by Spaces or not belong to [1-9]\n");
-        _exit(-1);
-    }
-    
-}
-void jugchar(char *p)
-{
-    if(*p < 'A' || *p > 'Z')
-    {
-        printf("the option can only be followed by uppercase\n");
-    }
-}
+
 void sighandler(int signum)
 {
     if(signum == SIGINT)
     {
-        sig_exit = 0;
+        sig_exit = 1;
     }
 }
+
 int main(int argc,char *argv[])
 
 {
     extern char *optarg;
     extern int optind, opterr, optopt;
     int ch;
-    //char *com_path = "/dev/ttyUSB0";
-    comport comport_info;
-    memset(&comport_info,0,sizeof(comport_info));
-    comport_info.parityb='N';
-    comport_info.datab = 8;
-    comport_info.baudspeed =115200;
-    comport_info.stopb = 1;
-    //字符数组不能用 = ，要用strncpy;
-    struct termios new_term,old_term;
     struct timeval timeout;
-    timeout.tv_sec = 10;//延时10s
-    timeout.tv_usec = 0;
+    struct termios old_term;
+    fd_set rset;
+    int fd_arr[3];
+    comport *comport_info = NULL;
+
+    int rv = -1;
+    char databuf[ARRMAX];
+
+    if(NULL == (comport_info = initComport()))
+    {
+        return -1;
+    }
 
     struct option opt[] = {
-        {"parityb",optional_argument,NULL,'p'},
+        {"paritybit",optional_argument,NULL,'p'},
         {"help",no_argument, NULL, 'h'},
-        {"baudspeed",optional_argument,NULL,'b'},
+        {"baudrate",optional_argument,NULL,'b'},
         {"datab",optional_argument,NULL,'d'},//{"",optional_argument,NULL,''},
-        {"stopb",optional_argument,NULL,'s'},
+        {"stopbit",optional_argument,NULL,'s'},
         {"path",required_argument,NULL,'f'},
         {0,         0,                 0,  0 }
     };
@@ -79,28 +67,30 @@ int main(int argc,char *argv[])
         switch(ch)
         {
             case 'p':
-                jugchar(optarg);
-                comport_info.parityb = *optarg;
+                comport_info->paritybit = *optarg;
                 break;
             case 'b':
-                jugnu(optarg);
-                comport_info.baudspeed = atoi(optarg);break;
+                comport_info->baudrate = atoi(optarg);
+                break;
             case 'd':
-                jugnu(optarg);
-                comport_info.datab = atoi(optarg);break;
+                comport_info->databit = atoi(optarg);
+                break;
             case 's':
-                jugnu(optarg);
-                comport_info.stopb = atoi(optarg);break;
+                comport_info->stopbit = atoi(optarg);
+                break;
             case 'f':
-                strncpy(comport_info.path,optarg,sizeof(comport_info.path));break;
+                strncpy(comport_info->path,optarg,sizeof(comport_info->path));//字符数组不能用 = ，要用strncpy;
+                break;
             case 'h':
-                showusage();return 0;
+                showusage();
+                return 0;
             default:
-                showusage();return -1;
+                showusage();
+                return -1;
         }
     }
 
-    if(comport_info.path[0] == 0)
+    if(comport_info->path[0] == 0)
     {
         printf("Please enter the correct serial port  path\n");
         showusage();
@@ -108,27 +98,27 @@ int main(int argc,char *argv[])
     }
 
     signal(SIGINT,sighandler);
-    openComport(&comport_info,&new_term);
-    new_term = old_term;
-    setComport(&comport_info,&new_term);
-    fd_set rset;
-    int fd_arr[3];
-    fd_arr[0] = STDIN_FILENO;
-    fd_arr[1] = comport_info.com_fd;
-    while(sig_exit)
+
+    if(openComport(comport_info,&old_term) < 0)
     {
+        return -1;
+    }
+
+    fd_arr[0] = STDIN_FILENO;
+    fd_arr[1] = comport_info->com_fd;
+    while(!sig_exit)
+    {
+        timeout.tv_sec = 60;//延时60s
+        timeout.tv_usec = 0;
+        /* 如果将时间放在函数体初始化，每次select计时值是上一次的剩余时间 */
         FD_ZERO(&rset);
         FD_SET(fd_arr[0],&rset);
         FD_SET(fd_arr[1],&rset);
-        char databuf[ARRMAX] = {0};
+        memset(&databuf,0,sizeof(databuf));
 
         printf("get data from the STDIN_FILENO,press enter to end...\n");
 
-        fgets(databuf,ARRMAX,stdin);
-        writeComport(comport_info.com_fd,databuf,ARRMAX);
-
-        int rv = -1;
-        rv = select(comport_info.com_fd+1, &rset, NULL,NULL, &timeout);  
+        rv = select((comport_info->com_fd)+1, &rset, NULL,NULL, &timeout);  
 
         if(rv < 0) 
         { 
@@ -144,28 +134,34 @@ int main(int argc,char *argv[])
 
         else
         {
-            //标准输入读发生
-            if(FD_ISSET(STDIN_FILENO,&rset))
-            {
-                if((read(STDIN_FILENO,databuf,ARRMAX)) < 0)
-                {
-                    return -1;
-                }
-                writeComport(comport_info.com_fd,databuf,ARRMAX);
-                readComport(comport_info.com_fd,databuf,ARRMAX);
-                printf("read data from serialport:\n%s",databuf);
-            }
-            
             //串口读事件发生
-            if(FD_ISSET(comport_info.com_fd,&rset))
+            if(FD_ISSET(comport_info->com_fd,&rset))
             {
-                readComport(comport_info.com_fd,databuf,ARRMAX);
-                printf("read data from serialport:\n%s",databuf);
+                if(readComport(comport_info->com_fd,databuf,ARRMAX) < 0)
+                {
+                    break;
+                }
+
+                printf("read data from serialport:\n%s\n",databuf);
+            }
+            //标准输入读发生
+            else
+            {
+                fgets(databuf,ARRMAX,stdin);//自动添加\0且不舍弃\n
+                databuf[strlen(databuf)-1] = '\0';
+                //printf("total %d bytes:%s\n",strlen(databuf),databuf);
+                if(writeComport(comport_info->com_fd,databuf,strlen(databuf)) < 0)
+                {
+                    break;
+                }
             }
         }
     }
+
     printf("program exit now and set the Save the serial port properties as the original properties...\n");
-    closeComport(comport_info.com_fd,&old_term);
+    closeComport(comport_info,&old_term);
+    //comport_term(comport_info);
+
     return 0;
 
 }
